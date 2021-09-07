6872bd1b24248ac99bc655800955ddbf838df746
fahrial2310 <88810688+fahrial2310@users.noreply.github.com>
2021-09-07 09:37:12 +0800

    Update config.py

--- AlvinMusicRobot/config.py
+++ AlvinMusicRobot/config.py
@@ -1,4 +1,4 @@
-# DAISYXMUSIC- Telegram bot project
+# AlvinMusicRobot- Telegram bot project
 # Copyright (C) 2021  Roj Serbest
 # Copyright (C) 2021  Inuka Asith
 # This program is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
+# Modified by alvin_junior
 
 import os
 from os import path
