56a677974f0a50780ff523123824bfe26d51cc48
fahrial2310 <88810688+fahrial2310@users.noreply.github.com>
2021-09-07 10:05:05 +0800

    Update voice_chat_ended.py

--- AlvinMusicRobot/modules/voice_chat_ended.py
+++ AlvinMusicRobot/modules/voice_chat_ended.py
@@ -2,8 +2,8 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DaisyXMusic.services.callsmusic.callsmusic import remove
-from DaisyXMusic.helpers.channelmusic import get_chat_id
+from AlvinMusicRobot.services.callsmusic.callsmusic import remove
+from AlvinMusicRobot.helpers.channelmusic import get_chat_id
 
 
 @Client.on_message(filters.voice_chat_ended)
