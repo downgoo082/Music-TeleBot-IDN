1d44b815c01aa3816d3f710d65e9e89170ecfc84
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-07 12:33:51 +0800

    Update youtube.py

--- AlvinMusicRobot/services/downloaders/youtube.py
+++ AlvinMusicRobot/services/downloaders/youtube.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Modified by Inukaasith
+# Modified by alvin_junior
 
 from os import path
 
