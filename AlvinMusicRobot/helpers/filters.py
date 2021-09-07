04beb4018c0ee3cc51ad59ddfe6eeb9d4da61a99
fahrial2310 <88810688+fahrial2310@users.noreply.github.com>
2021-09-07 09:41:29 +0800

    Update filters.py

--- AlvinMusicRobot/helpers/filters.py
+++ AlvinMusicRobot/helpers/filters.py
@@ -20,7 +20,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from DaisyXMusic.config import COMMAND_PREFIXES
+from AlvinMusicRobot.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
