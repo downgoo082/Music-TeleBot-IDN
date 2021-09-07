fecbebd0123507d10252f11afa7798a206256804
fahrial2310 <88810688+fahrial2310@users.noreply.github.com>
2021-09-07 09:39:49 +0800

    Update __init__.py

--- AlvinMusicRobot/function/__init__.py
+++ AlvinMusicRobot/function/__init__.py
@@ -1,5 +1,5 @@
-from DaisyXMusic.function.admins import admins
-from DaisyXMusic.function.admins import get
-from DaisyXMusic.function.admins import set
+from AlvinMusicRobot.function.admins import admins
+from AlvinMusicRobot.function.admins import get
+from AlvinMusicRobot.function.admins import set
 
 __all__ = ["set", "get", "admins"]
